d71d86ca994ece73a4da3d4bb2612e33e7ef3a5c
Suraj Laxman Jadhav <116006375+Surajjadhav1334@users.noreply.github.com>
2024-05-24 19:44:04 +0530

    React

---
+++ Full Stack Basic/Backend/server.js
@@ -0,0 +1,13 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('server is empty');
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`serve at http://localhost${port} `);
+});
