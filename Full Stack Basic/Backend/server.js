41fb3e6be2b554fb5543756027a7423a16dbfd64
Suraj Laxman Jadhav <116006375+Surajjadhav1334@users.noreply.github.com>
2024-05-26 12:45:36 +0530

    react

--- Full Stack Basic/Backend/server.js
+++ Full Stack Basic/Backend/server.js
@@ -6,6 +6,37 @@ app.get('/', (req, res) => {
     res.send('server is empty');
 });
 
+app.get('/jokes', (req, res) => {
+    const jokes = [
+        {
+            id : 1,
+            title: 'A Joke',
+            content: 'This is a joke'
+        },
+        {
+            id : 2,
+            title: 'Another Joke',
+            content: 'This is a joke'
+        },
+        {
+            id : 3,
+            title: 'A Third Joke',
+            content: 'This is a joke'
+        },
+        {
+            id : 4,
+            title: 'A Fourth Joke',
+            content: 'This is a joke'
+        },
+        {
+            id : 5,
+            title: 'A Fifth Joke',
+            content: 'This is a joke'
+        },
+    ];
+    res.send(jokes);
+})
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
